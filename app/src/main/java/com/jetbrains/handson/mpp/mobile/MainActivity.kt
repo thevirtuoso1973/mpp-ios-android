a1ca1a830985e019c233824ccdbf936041e14a7f
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-07-29 16:50:14 +0200

    add sources form default Android project via AndroidStudio 3.4

---
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.handson.mpp.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
