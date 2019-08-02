49cd15d63e2cda53423ca8d5b88844f842a82e63
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-08-02 17:05:25 +0200

    call Kotlin code from Swift

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -1,20 +1,15 @@
-//
-//  ViewController.swift
-//  KotlinIOS
-//
-//  Created by Evgeny Petrenko on 02.08.2019.
-//  Copyright © 2019 Evgeny Petrenko. All rights reserved.
-//
-
 import UIKit
+import SharedCode
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
     }
-
-
 }
-
