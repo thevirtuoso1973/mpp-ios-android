3e061df78afb7d2727b0b0b06f436abd92ae1ea7
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-08-02 12:36:08 +0200

    add Xcode project

---
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KotlinIOS
+//
+//  Created by Evgeny Petrenko on 02.08.2019.
+//  Copyright © 2019 Evgeny Petrenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
