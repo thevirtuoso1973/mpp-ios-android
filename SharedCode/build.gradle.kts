ff5bbc8efd4e3bf2ae842e9a111de5f86bc25c8c
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-08-02 13:39:37 +0200

    include Xcode binaries preparation

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 }
 
 kotlin {
+    //select iOS target platform depending on the Xcode environment variables
     val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
         if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
             ::iosArm64
@@ -29,3 +30,27 @@ kotlin {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
 }
+
+
+val packForXCode by tasks.creating(Sync::class) {
+    group = "build"
+
+    //selecting the right configuration for the iOS framework depending on the Xcode environment variables
+    val mode = project.findProperty("XCODE_CONFIGURATION")?.toString() ?: "DEBUG"
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
+
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+
+    val targetDir = File(buildDir, "xcode-frameworks")
+    from({ framework.outputDirectory })
+    into(targetDir)
+
+    doLast {
+        val gradlew = File(targetDir, "gradlew")
+        gradlew.writeText("#!/bin/bash\nexport 'JAVA_HOME=${System.getProperty("java.home")}'\ncd '${rootProject.rootDir}'\n./gradlew \$@\n")
+        gradlew.setExecutable(true)
+    }
+}
+
+tasks.getByName("build").dependsOn(packForXCode)
