11804f0dbba50df5bc138a5e548d3081dea8e75a
Abel Shields <abel.shields@softwire.com>
2020-07-13 12:42:04 +0100

    Updated gradle

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -1,20 +1,59 @@
 import UIKit
 import SharedCode
 
+class PickerDelegate: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+    private var pickerOptions: Array<String> = ["a", "b", "c"]
+    
+    func PickerDelegate() {
+    }
+    
+    func pickerView(_: UIPickerView, titleForRow: Int, forComponent: Int) -> String? {
+        return pickerOptions[titleForRow]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerOptions.count
+    }
+    
+    func setPickerOptions(newOptions: Array<String>) {
+        pickerOptions = newOptions
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet private var label: UILabel!
-
+    @IBOutlet private var firstStationPicker: UIPickerView!
+    @IBOutlet private var secondStationPicker: UIPickerView!
+    @IBOutlet private var submitButton: UIButton!
+    
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
+    private let stationPickerDelegate = PickerDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewTaken(view: self)
+        firstStationPicker.delegate = stationPickerDelegate
+        firstStationPicker.dataSource = stationPickerDelegate
+        secondStationPicker.delegate = stationPickerDelegate
+        secondStationPicker.dataSource = stationPickerDelegate
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        presenter.onSubmitButtonPressed(view: self)
     }
 }
 
 extension ViewController: ApplicationContractView {
     func setLabel(text: String) {
         label.text = text
+    }
+    func setStations(stations: Array<String>) {
+        stationPickerDelegate.setPickerOptions(newOptions: stations)
     }
 }
