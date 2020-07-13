8d848a923b1fd6826621ba87a08490e4434585fe
Chris <Chris.Tomy@softwire.com>
2020-07-13 13:24:41 +0100

    add stations dropdown for android

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.jetbrains.handson.mpp.mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
@@ -14,7 +15,14 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
         presenter.onViewTaken(this)
     }
 
-    override fun setLabel(text: String) {
-        findViewById<TextView>(R.id.main_text).text = text
+    override fun setStations(stations: Array<String>) {
+        val arrayAdapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_spinner_dropdown_item,
+            stations
+        )
+        val spinnerFrom: Spinner = findViewById(R.id.spinner_from)
+        val spinnerTo: Spinner = findViewById(R.id.spinner_to)
+        spinnerFrom.adapter = arrayAdapter
+        spinnerTo.adapter = arrayAdapter
     }
 }
